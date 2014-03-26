6ce4f2f3c5c54a4c722352f714642de6b2744012
Lieke Boon <lieke@kabisa.nl>
2014-03-26 09:17:42 +0100

    test app first commit

--- db/migrate/20140325195919_rename_column.rb
+++ db/migrate/20140325195919_rename_column.rb
@@ -2,4 +2,8 @@ class RenameColumn < ActiveRecord::Migration
     def self.up
       rename_column :students, :student_id, :student_number
     end
+
+    def self.down
+
+    end
 end
--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -4,13 +4,13 @@ FactoryGirl.define do
     s.student_number { [1,5,8].sample }
     s.name { Faker::Name.name }
     s.degree 7.5
-    s.scriptie Faker::Lorem.paragraphs(5)
+    s.scriptie { Faker::Lorem.paragraphs(5) }
   end
 
   factory :studentgrade do |s|
     s.grade {['A', 'B', 'C', 'D', 'E', 'F'].sample}
     s.student_id {Faker::Number.number(3)}
-    s.date DateTime.now
+    s.date { rand(30).days.ago }
   end
 
 
